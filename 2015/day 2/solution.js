7e7990cd7a7ec46d5cd987856f438e196d98aa66
Daniil Astapenko <astapenko.daniil.03@gmail.com>
2025-07-09 16:46:37 +0200

    Solve day 2, 2015

---
+++ 2015/day 2/solution.js
@@ -0,0 +1,23 @@
+const { readInput } = require("../../utils");
+
+const data = readInput("./2015/day 2/input.txt");
+const list = data.split("\r\n");
+let total = 0;
+let totalRibbon = 0;
+
+for (let i = 0; i < list.length; i++) {
+    let [l, w, h] = list[i].split("x");
+    l = Number(l);
+    w = Number(w);
+    h = Number(h);
+
+    const surfaceArea = 2*l*w + 2*w*h + 2*h*l;
+    const smallestSideArea = Math.min(l*w, w*h, h*l);
+    total += surfaceArea + smallestSideArea;
+
+    const [minDim1, minDim2] = [l, w, h].sort((a, b) => a - b);
+    totalRibbon += minDim1+minDim1 + minDim2+minDim2 + l*w*h;
+}
+
+console.log(total);
+console.log(totalRibbon);
